7e3d7c1d9f4653e305cd5ad087373e36815e9981
MC-MOOGLE <dmitriybosoy@gmail.com>
2024-01-27 15:43:24 +0300

    cpp 6

--- CPP/24.01.26/6-1.cpp
+++ CPP/24.01.26/6-1.cpp
@@ -1,9 +1,10 @@
-п»ї#include <iostream>
+#include <iostream>
 #include <string>
 #include <Windows.h>
 #include <stdio.h>
 #include <iomanip>
 #include <regex>
+#include <map>
 
 #define RU setlocale(LC_ALL, "ru-RU");
 #define EN setlocale(LC_ALL, "en-EN");
@@ -21,26 +22,26 @@ void printTask(int n) {
     switch (n)
     {
     case 1:
-        task = { R"delimeter(РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, СЂРµР°Р»РёР·СѓСЋС‰СѓСЋ РїСЂРѕС†РµРґСѓСЂС‹ Р·Р°РєР°Р·Р° Рё СЂР°СЃС‡РµС‚Р° РµРіРѕ СЃСѓРјРјС‹ РІ РјРёРЅРё - РїРёС†С†РµСЂРёРё.Р’ РјРµРЅСЋ РїРёС†С†РµСЂРёРё РїСЂРµРґСѓСЃРјРѕС‚СЂРµРЅРѕ 4 РІРёРґР° РїРёС†С†С‹ Рё С‚СЂРё РІРёРґР° РЅР°РїРёС‚РєРѕРІ.
-РџРѕР»СЊР·РѕРІР°С‚РµР»СЋ РІС‹РІРѕРґРёС‚СЃСЏ РјРµРЅСЋ (РІРЅР°С‡Р°Р»Рµ РїРёС†С†С‹, РїРѕС‚РѕРј РЅР°РїРёС‚РєРё), СЃРѕРґРµСЂР¶Р°С‰РµРµ РєРѕРґ Рё РЅР°Р·РІР°РЅРёРµ. РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РІРІРѕРґРёС‚ РєРѕРґ Р¶РµР»Р°РµРјРѕРіРѕ РїСЂРѕРґСѓРєС‚Р°, РїРѕСЃР»Рµ С‡РµРіРѕ РІРІРѕРґРёС‚ РєРѕР»РёС‡РµСЃС‚РІРѕ РµРґРёРЅРёС† РґР°РЅРЅРѕРіРѕ РїСЂРѕРґСѓРєС‚Р°. Р’ РїРёС†С†РµСЂРёРё РїСЂРµРґСѓСЃРјРѕС‚СЂРµРЅС‹ РґРІР° РІРёРґР° СЃРєРёРґРѕРє:
-    вЂў РµСЃР»Рё РѕР±С‰Р°СЏ СЃСѓРјРјР° Р·Р°РєР°Р·Р° Р±РѕР»РµРµ 50$, С‚Рѕ СЂР°Р·РјРµСЂ СЃРєРёРґРєРё СЃРѕСЃС‚Р°РІР»СЏРµС‚ 20% РѕС‚ СЃСѓРјРјС‹ Р·Р°РєР°Р·Р°;
-    вЂў РєР°Р¶РґР°СЏ РїСЏС‚Р°СЏ РїРёС†С†Р° вЂ” РІ РїРѕРґР°СЂРѕРє;
-    вЂў РґР»СЏ РЅР°РїРёС‚РєРѕРІ СЃ С†РµРЅРѕР№ Р±РѕР»РµРµ 2$, РµСЃР»Рё РєРѕР»РёС‡РµСЃС‚РІРѕ РІ Р·Р°РєР°Р·Рµ Р±РѕР»РµРµ С‚СЂРµС…, С‚Рѕ СЃРєРёРґРєР° 15% (С‚РѕР»СЊРєРѕ РЅР° РЅР°РїРёС‚РєРё, Р° РЅРµ РЅР° РІРµСЃСЊ Р·Р°РєР°Р·).
-Р’С‹РІРµСЃС‚Рё РїРѕР»СЊР·РѕРІР°С‚РµР»СЋ В«С‡РµРєВ» РґР»СЏ РѕРїР»Р°С‚С‹ РІ РІРёРґРµ:
-РќР°Р·РІР°РЅРёРµ1 вЂ” РєРѕР»РёС‡РµСЃС‚РІРѕ1 вЂ” С†РµРЅР°1
-РќР°Р·РІР°РЅРёРµ2 вЂ” РєРѕР»РёС‡РµСЃС‚РІРѕ2 вЂ” С†РµРЅР°2
+        task = { R"delimeter(Написать программу, реализующую процедуры заказа и расчета его суммы в мини - пиццерии.В меню пиццерии предусмотрено 4 вида пиццы и три вида напитков.
+Пользователю выводится меню (вначале пиццы, потом напитки), содержащее код и название. Пользователь вводит код желаемого продукта, после чего вводит количество единиц данного продукта. В пиццерии предусмотрены два вида скидок:
+    • если общая сумма заказа более 50$, то размер скидки составляет 20% от суммы заказа;
+    • каждая пятая пицца — в подарок;
+    • для напитков с ценой более 2$, если количество в заказе более трех, то скидка 15% (только на напитки, а не на весь заказ).
+Вывести пользователю «чек» для оплаты в виде:
+Название1 — количество1 — цена1
+Название2 — количество2 — цена2
 ...
-РќР°Р·РІР°РЅРёРµN вЂ” РєРѕР»РёС‡РµСЃС‚РІРѕN вЂ” С†РµРЅР°N
-РС‚РѕРіРѕ Рє РѕРїР»Р°С‚Рµ: $XXX.
-Р’РјРµСЃС‚Рѕ XXX вЂ“ РґРѕР»Р¶РЅР° Р±С‹С‚СЊ СЃС‚РѕРёРјРѕСЃС‚СЊ РІСЃРµРіРѕ Р·Р°РєР°Р·Р°.)delimeter" };
+НазваниеN — количествоN — ценаN
+Итого к оплате: $XXX.
+Вместо XXX – должна быть стоимость всего заказа.)delimeter" };
         break;
     case 2:
-        task = { R"delimeter(РќРµРѕР±С…РѕРґРёРјРѕ РЅР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, СЂР°СЃСЃС‡РёС‚С‹РІР°СЋС‰СѓСЋ Р·Р°СЂРїР»Р°С‚Сѓ С‚СЂРµС… РјРµРЅРµРґР¶РµСЂРѕРІ. Р‘Р°Р·РѕРІР°СЏ СЃС‚Р°РІРєР° РјРµРЅРµРґР¶РµСЂР° СЃРѕСЃС‚Р°РІР»СЏРµС‚ 60.000 СЂСѓР±. + РїСЂРѕС†РµРЅС‚ РѕС‚ РїСЂРѕРґР°Р¶, СЂР°СЃСЃС‡РёС‚С‹РІР°РµРјС‹Р№ СЃР»РµРґСѓСЋС‰РёРј РѕР±СЂР°Р·РѕРј:
-    вЂў РїСЂРѕРґР°Р¶Рё РґРѕ 300.000 СЂСѓР±. вЂ” 3%,
-    вЂў РѕС‚ 300.000 РґРѕ 700.000 СЂСѓР±. вЂ” 5%,
-    вЂў СЃРІС‹С€Рµ 700.000 вЂ“ 8%.
-РџРѕ РѕРєРѕРЅС‡Р°РЅРёРё СЂР°СЃС‡РµС‚Р° РІСЃРµС… Р·Р°СЂРїР»Р°С‚ РЅРµРѕР±С…РѕРґРёРјРѕ РѕРїСЂРµРґРµР»РёС‚СЊ РЅР°РёР±РѕР»РµРµ С†РµРЅРЅРѕРіРѕ СЃРѕС‚СЂСѓРґРЅРёРєР° (РІС‹РїРѕР»РЅРёРІС€РµРіРѕ Р±РѕР»СЊС€Рµ РІСЃРµС… РїСЂРѕРґР°Р¶) Рё РІС‹РґР°С‚СЊ РµРјСѓ РїСЂРµРјРёСЋ РІ СЂР°Р·РјРµСЂРµ РґРІСѓС… Р±Р°Р·РѕРІС‹С… СЃС‚Р°РІРѕРє.
-РџСЂРёРјРµС‡Р°РЅРёРµ: СѓСЂРѕРІРµРЅСЊ РїСЂРѕРґР°Р¶ Сѓ РІСЃРµС… С‚СЂРµС… РјРµРЅРµРґР¶РµСЂРѕРІ СЂР°Р·РЅС‹Р№.)delimeter" };
+        task = { R"delimeter(Необходимо написать программу, рассчитывающую зарплату трех менеджеров. Базовая ставка менеджера составляет 60.000 руб. + процент от продаж, рассчитываемый следующим образом:
+    • продажи до 300.000 руб. — 3%,
+    • от 300.000 до 700.000 руб. — 5%,
+    • свыше 700.000 – 8%.
+По окончании расчета всех зарплат необходимо определить наиболее ценного сотрудника (выполнившего больше всех продаж) и выдать ему премию в размере двух базовых ставок.
+Примечание: уровень продаж у всех трех менеджеров разный.)delimeter" };
         break;
     default:
         break;
@@ -50,67 +51,146 @@ void printTask(int n) {
     EN;
 };
 
-class Menu
-{
+class Food {
 public:
-    void showMenu() {
-        int i = 0;
-        Color(2)
-        cout << "Name\t\t" << "Price\t" << "Code\t" << endl;
-        Color(7)
-        cout << "Pizza : " << endl;
-        for (string position : pizza) {
-            i++;
-            cout << position << tab << i * 75 << tab << "P" << i << endl;
-        }
-        i = 0;
-        cout << "\nDrinks : " << endl;
-        for (string position : drinks) {
-            i++;
-            cout << position << tab << tab << i * 75 << tab << "D" << i << endl;
+    bool Pizza;
+    string Name;
+    string Code;
+    double Price;
+    int Amount = 0;
+    double total = 0;
+    Food(bool v = false, string x = "Not found", string y = "", double z = 0) {
+        Pizza = v;
+        Name = x;
+        Code = y;
+        Price = z;
+    };
+};
+
+void t1() {
+    /*
+    Всё в этом мире либо картошка... либо не картошка
+
+    В моем случае все либо пицца, либо не пицца
+    */
+
+    Food pizza1(true, "Pepperonni", "P1", 2.50);
+    Food pizza2(true, "Margherita", "P2", 1.75);
+    Food pizza3(true, "4 cheese ", "P3", 2);
+    Food pizza4(true, "Hawaiian ", "P4", 3);
+
+    Food drink1(false, "Cola    ", "D1", 2.5);
+    Food drink2(false, "Pepsi   ", "D2", 2.25);
+    Food drink3(false, "Water   ", "D3", 0.3);
+
+    Food full[] = { pizza1, pizza2, pizza3, pizza4, drink1, drink2, drink3 };
+
+    Color(2);
+    cout << "Name\t\tCode\tPrice" << endl;
+    Color(7);
+    for (Food c : full) {
+        cout << c.Name << "\t" << c.Code << "\t" << c.Price << "$" << endl;
+    }
+
+    string pizzaCode, drinkCode;
+    int pizzaQ, drinkQ;
+    Food pizza, drink;
+    cout << "Enter pizza code >> ";
+    cin >> pizzaCode;
+    cout << "Enter pizza amount >> ";
+    cin >> pizzaQ;
+    for (Food c : full) {
+        if (c.Code == pizzaCode) {
+            pizza = c;
+            pizza.Amount = pizzaQ;
         }
     }
 
-    void getPosition(string code) {
-        regex numberRegex("[0-9]+");
-
-        string letter = regex_replace(code, numberRegex, "");
-        int number = exctractNumber(code);
-
-        cout << letter << " " << number << endl;
-
-    }
-private:
-    string pizza[4] = { "Pepperonni", "4 cheese", "Margherita", "Hawaiian" };
-    string drinks[3] = { "Cola", "Pepsi", "Water" };
-
-    int exctractNumber(string str) {
-        string tmpNumber;
-        for (char c : str) {
-            int num;
-            try {
-                num = stoi(string(1, c));
-                tmpNumber += to_string(num);
-            }
-            catch (invalid_argument&) {}
+    cout << "Enter drinks code >> ";
+    cin >> drinkCode;
+    cout << "Enter drink amount >> ";
+    cin >> drinkQ;
+    for (Food c : full) {  
+        if (c.Code == drinkCode) {
+            drink = c;
+            drink.Amount = drinkQ;
         }
-        return stoi(tmpNumber);
+    }
+
+    pizza.total = pizza.Price * pizza.Amount;
+    drink.total = drink.Price * drink.Amount;
+
+    if (pizza.Amount >= 5) {
+        pizza.total -= pizza.Price * (pizza.Amount / 5);
+    }
+
+    if (drink.Price > 2 && drink.Amount > 3) {
+        drink.total *= 0.85;
+    }
+
+    double total = pizza.total + drink.total;
+    if (total > 50) {
+        total *= 0.8;
+    }
+
+    cout << "\nTotal:" << endl;
+    cout << "Name       " << "Amount    " << "Price " << endl
+        << pizza.Name << "\t" << pizza.Amount << "\t" << pizza.total << endl
+        << drink.Name << "\t" << drink.Amount << "\t" << drink.total << endl;
+
+    cout << "Total: " << total;
+}
+
+class Manager {
+public:
+    const int baseSalary = 60'000;
+    int salary = baseSalary;
+    int volume = 0;
+
+    void addSells(int sellVolume) {
+        if (sellVolume < 300'000) {
+            salary += sellVolume * 0.03;
+        }
+        else if (sellVolume < 700'000) {
+            salary += sellVolume * 0.05;
+        }
+        else {
+            salary += sellVolume * 0.08;
+        }
+        volume = sellVolume;
+    }
+
+    void genSells(int number) {
+        cout << "Enter manager #" << number << " sells volume >> ";
+        cin >> volume;
+        addSells(volume);
     }
 };
 
-
-void t1() {
-    Menu menu;
-    menu.showMenu();
-    menu.getPosition("A2");
-}
-
 void t2() {
+    Manager manager1, manager2, manager3;
+    manager1.genSells(1);
+    manager2.genSells(2);
+    manager3.genSells(3);
 
-}
+    Manager ManagerList[] = { manager1, manager2, manager3 };
 
-void t3() {
+    int bestVolume = 0;
 
+    for (int i = 0; i < 3; i ++) {
+        Manager c = ManagerList[i];
+        if (c.volume > bestVolume) {
+            bestVolume = c.volume;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        Manager c = ManagerList[i];
+        if (c.volume == bestVolume) {
+            c.salary += c.baseSalary * 2;
+        }
+        cout << "Manager #" << i + 1 << " salary << " << c.salary << endl;
+    }
 }
 
 int main() {
